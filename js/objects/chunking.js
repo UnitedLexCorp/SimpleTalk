3778b1a71522e2800260808a2399fd905fa0496e
Eric Gade <eric.gade@gmail.com>
2020-09-25 18:24:06 -0400

    Alternate chunking pattern. String and Array implementations
    With this branch/commit I am providing an alternate pattern for the
    chunking API and demonstrating how it might work using Arrays and
    Strings as the initial examples.
    
    The key difference between this method and the preceding one is that a
    'chunker' on an object is not itself a method, but an object with
    three required methods:
    1. getAll -- returns a list/array of all the chunks of the specified
    kind (ie, all lines of text in a 'line' chunking of a string)
    2. getAt -- Returns a specific chunk at a given index of similar
    chunks on the source object (ie, line 3 of string)
    3. setAt -- Returns a *new copy of the original object* where the
    given chunk at the specified index is changed to some new value the
    consumer passes in (`ie set character 4 of text to "p"` becomes
    something like "hello".chunkers.character.setAt(3, "p"), giving us a
    new string of "helpo")
    
    I am already convinced that each object needs to know how to "chunk"
    itself, and should specify which types of chunks it knows how to make
    of itself. Each will also need to know how to re-set its own value(s)
    based on setting chunks of a given kind at a given index. Because
    strings are immutable here in JS, we have to return a copy of the
    object in setAt(). Should that behavior be universal? What if the
    object being chunked is a more complex object -- do we really want to
    copy the whole thing when setting just a small part of it?

--- js/objects/chunking.js
+++ js/objects/chunking.js
@@ -2,48 +2,150 @@
  * Utilities for SimpleTalk Chunking
  */
 
+/* Helpers for Binding Chunker functions */
+function chunkBy(aChunkName){
+    if(!this.chunkers || this.chunkers == undefined){
+        return null;
+    }
+
+    let chunker = this.chunkers[aChunkName];
+    if(chunker){
+        return chunker.getAll.bind(this)();
+    }
+    return null;
+};
+
+function chunkAt(aChunkName, anIndex){
+    if(!this.chunkers || this.chunkers == undefined){
+        return null;
+    }
+
+    let chunker = this.chunkers[aChunkName];
+    if(chunker){
+        return chunker.getAt.bind(this)(anIndex);
+    }
+    return null;
+};
+
 /* JS String Chunking */
-String.prototype.chunkers = {
-    line: function(){
+String.prototype.chunkers = {};
+String.prototype.chunkBy = chunkBy;
+String.prototype.chunkAt = chunkAt;
+
+// Lines
+String.prototype.chunkers.line = {
+    getAll: function(){
         return this.split("\n");
     },
 
-    character: function(){
-        let chars = [];
-        for(let i = 0; i < this.length; i++){
-            chars.push(
-                this.charAt(i)
-            );
+    getAt(anIndex){
+        let all = this.split("\n");
+        if(anIndex >= all.length){
+            return null;
         }
-        return chars;
+        return all[anIndex];
+    },
+
+    setAt(anIndex, aString){
+        let all = this.split("\n");
+        let diff = anIndex - this.length;
+        if(diff >= 0){
+            let extraNewlines = "";
+            for(let i = 0; i < diff; i++){
+                extraNewlines += "\n";
+            }
+            return `${this}${extraNewlines}${aString}`;
+        }
+        all[anIndex] = aString;
+        return all.join("\n");
     }
 };
 
-String.prototype.chunkBy = function(aChunkType){
-    if(!this.chunkers){
-        return null;
-    }
-    if(!Object.keys(this.chunkers).includes(aChunkType)){
-        return null;
-    }
+// Words
+String.prototype.chunkers.word = {
+    getAll: function(){
+        return this.split(" ");
+    },
 
-    return this.chunkers[aChunkType].bind(this)();
+    getAt(anIndex){
+        let all = this.split(" ");
+        if(anIndex >= all.length){
+            return null;
+        }
+        return all[anIndex];
+    },
+
+    setAt(anIndex, aString){
+        let all = this.split(" ");
+        let newSpaces = "";
+        let diff = anIndex - all.length;
+        if(diff >= 0){
+            for(let i = 0; i < diff; i++){
+                newSpaces += "\n";
+            }
+            return `${this}${newSpaces}${aString}`;
+        }
+        all[anIndex] = aString;
+        return all.join("\n");
+    }
+};
+
+// Characters
+String.prototype.chunkers.character = {
+    getAll: function(){
+        let chars = [];
+        for(let i = 0; i < this.length; i++){
+            chars.push(this.charAt(i));
+        }
+        return chars;
+    },
+
+    getAt(anIndex){
+        if(anIndex >= this.length){
+            return null;
+        }
+        return this.charAt(anIndex);
+    },
+
+    setAt(anIndex, aCharacter){
+        if(anIndex >= this.length){
+            return null;
+        }
+        let all = [];
+        for(let i = 0; i < this.length; i++){
+            all.push(this.charAt(i));
+        }
+        all[anIndex] = aCharacter;
+        return all.join("");
+    }
 };
 
 /* JS Array Chunking */
-Array.prototype.chunkers = {
-    item: function(){
-        return this;
-    }
-};
+Array.prototype.chunkers = {};
+Array.prototype.chunkBy = chunkBy;
+Array.prototype.chunkAt = chunkAt;
 
-Array.prototype.chunkBy = function(aChunkType){
-    if(!this.chunkers){
-        return null;
-    }
-    if(!Object.keys(this.chunkers).includes(aChunkType)){
-        return null;
-    }
+Array.prototype.chunkers.item = {
+    getAll: function(){
+        return this.slice();
+    },
 
-    return this.chunkers[aChunkType].bind(this)();
+    getAt: function(anIndex){
+        if(anIndex >= this.length){
+            return null;
+        }
+        return this[anIndex];
+    },
+
+    setAt(anIndex, anObject){
+        let all = this.slice();
+        let diff = anIndex - all.length;
+        if(diff >= 0){
+            for(let i = 0; i < diff; i++){
+                all.push(null);
+            }
+        }
+        all.push(anObject);
+        return all;
+    }
 };
